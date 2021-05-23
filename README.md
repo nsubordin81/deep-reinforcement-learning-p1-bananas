e9a89239f0e0efe666d02e6df3fdb985b57bdaec
Taylor Bird <nsubordin81@gmail.com>
2021-05-23 17:27:39 -0400

    type: chore and docs
    Now that I've ensured the environment would work properly, I wanted to
    find a more seamless way to install the dependencies to make it simpler
    for the project evaluators. I found github issue because pypi had
    stopped supporting the version of pytorch that this project used, so I
    chose to download a local wheel file from pytorch for the version needed
    and install it through requirements.txt. I also didn't end up needing
    setup.py after this so I removed that too. The build directory had many
    things in it but when I deleted themt he environment still worked so I
    just trimmed the overhead there  until I find I need it.
    
    addresses: having a project that is easy to run.

--- README.md
+++ README.md
@@ -12,7 +12,25 @@ Specifically, the task will be considered 'solved' when the agent is able to mai
 
 ## Instructions For Installing Dependencies Or Downloading Files necessary To Run My Project
 
-<TBD>
+To run the Unity and train the agent there are a few dependencies to be installed. Most of the setup leverages the python/ directory 
+of the Udacity Deep Reinforcement Learning course repository, Only the pytorch version used was no longer listed in Pypi so I 
+downloaded it as a local wheel and saved it into what is mostly a copy of that directory. 
+First of all, this was tested on a 64 bit Windows machine, the platform shouldn't matter but I haven't tested on OSX or Linux. 
+
+The runtime environment for the project requires python version 3.6 and the unityagent package depends on an old version of pytorch. The way I ensured I could install these particular versions of python and the packages was using conda, though I imagine I could have done it
+with a local python distribution or a virtual environment. I recommend conda. 
+
+download it for your appropriate platform from here https://www.anaconda.com/products/individual, you probably only need the conda package manager for this, especially if you are OSX or Linux but I ran my examples from within their powershell client, so I'm pointing to that here. Now you are ready to install your dependencies. . . 
+
+create a conda environment `conda create -n <name it something> python==3.6`
+now install the dependencies, you need to run this from within the python/ directory because the local wheel's path is relative to that: 
+`<from repo root> cd python`
+`pip install -r requirements.txt # this will download the old pytorch version as well as unity agents, removed the setup.py as we don't need to package anything anymore`
+
+a good way to test that you have been able to successfully install dependencies os to go back to the root of the project and run:
+`udacity_navigation_code.py`
+you should see the udacity Bananas environment fire up inside a new unity mlagent application window and an agent will randomly take actions for an episode.
+
 
 ## I Have Instructions On How To Run The Project, How To Train The Agent In Other Words
 
--- udacity_navigation_code.py
+++ udacity_navigation_code.py
@@ -9,7 +9,47 @@ def main():
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
 
-    print(brain)
+    # reset the environment
+    env_info = env.reset(train_mode=True)[brain_name]
+
+    # number of agents in the environment
+    print('number of agents:', len(env_info.agents))
+
+    # number of actions (aside I find it interesting the action space is on the brain not env_info)
+    # this is because more or less the brains are entities that can take actions in the environment
+    # going to how you would code a unity game they have controller objects and you can either
+    # have brains that are player controlled or  agent controlled. So we only have one agent, 
+    # and no players, so we just want to have the one brain. But as you can see below, 
+    # you have to specify which brain is taking the action in the environment because there
+    # could be more than one. 
+    action_size = brain.vector_action_space_size
+    print('Number of actions:', action_size)
+
+    # example the state space
+    state = env_info.vector_observations[0]
+    print('States look like:', state)
+    state_size = len(state)
+    print('States have length:', state_size)
+
+    env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+    state - env_info.vector_observations[0]            # get the current state
+    score = 0                                          # initilize the score
+
+    # this code (this whole file is provided by Udactiy) 
+    # has the agent go through a single episode in the environment taking uniform random actions
+    while True:
+        action = np.random.randint(action_size)        # select an action at random
+        env_info = env.step(action)[brain_name]        # send the action to the environment
+        next_state = env_info.vector_observations[0]   # get the next state
+        reward = env_info.rewards[0]                   # get the reward
+        done = env_info.local_done[0]                  # see if the episode is over
+        score += reward                                # update the score
+        state = next_state                             # roll over the state to the next time step
+        if done:                                       # exit the loop if the episode is done
+            break
+
+    print("Score: {}".format(score))
+
 
 
 if __name__ == "__main__":
