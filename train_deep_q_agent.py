01c6f6d88915ae1200e5ddc307c1a2e4970c3c05
Taylor Bird <nsubordin81@gmail.com>
2021-05-24 19:31:57 -0400

    type: feature initial training script
    type: feature
    
    setting up initial script for model training. I'm going to resist the urge to refactor prematurely, see how it goes.
    
    addresses: having a file to work from.

---
+++ train_deep_q_agent.py
@@ -0,0 +1,16 @@
+import random
+from collections import deque
+
+import gym
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+# entry point for training
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    do main()
